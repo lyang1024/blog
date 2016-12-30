2e50024c52b33e097f2427ccef2720bd394f954b
lyangg <lqyang210@gmail.com>
2016-12-29 16:09:21 -0800

    register:routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 */
+var crypto = require('crypto'),
+	User = require('../models/user.js');
+
 module.exports = function(app) {
 	app.get('/',function (req,res) {
 		res.render('index',{title:'Home Page'});
@@ -16,6 +19,40 @@ module.exports = function(app) {
 		res.render('reg',{title:'Register'});
 	});
 	app.post('/reg',function(req, res) {
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+		if (password_re != password) {
+			req.flash('error', 'Password does not match!');
+			return res.redirect('/reg');
+		}
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: name,
+			password: password,
+			email: req.body.email
+		});
+		User.get(newUser.name, function(err, user) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			if (user) {
+				req.flash('error', 'User name already exits!');
+				return res.redirect('/reg');
+			}
+			newUser.save(function (err, user) {
+				if (err) {
+					req.flash('error',err);
+					return res.redirect('/reg');
+				}
+				req.session.user = user;
+				req.flash('success', 'Successfully registered!');
+				res.redirect('/');
+			});
+		});
+	});
 	});
 	app.get('/login',function(req, res) {
 		res.render('login',{title: 'Login'});
